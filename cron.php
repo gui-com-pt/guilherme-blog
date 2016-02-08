40a322e564d17cb373f772dc36196b0364e5b0a0
Guilherme Cardoso <email@guilhermecardoso.pt>
2016-02-08 06:43:51 +0000

    admin

--- cron.php
+++ cron.php
@@ -1,6 +1,6 @@
 <?hh
 
-use Pi\AppHost;
+use Pi\MockHost;
 use Pi\Odm\OdmPlugin;
 use Pi\Interfaces\IContainer;
 use Pi\FileSystem\FileSystemService;
@@ -15,19 +15,20 @@ use SpotEvents\ServiceInterface\Data\EventRepository;
 use SpotEvents\ServiceInterface\Data\NutritionRepository;
 use SpotEvents\ServiceInterface\Data\ModalityRepository;
 
-require '../vendor/autoload.php';
+require 'vendor/autoload.php';
 
 class MongoCursorException extends \Exception {
 
 }
 
-class DemoHost extends AppHost {
+class LocalHost extends MockHost {
 
   public function configure(IContainer $container)
   {
     header('P3P: policyref="/w3c/p3p.xml", CP="ALL IND DSP COR ADM CONo CUR CUSo IVAo IVDo PSA PSD TAI TELo OUR SAMo CNT COM INT NAV ONL PHY PRE PUR UNI"');
-  	$this->config()->domain('viseu.ovh');
-    $this->config()->protocol('https');
+  	$this->config()->domain('localhost');
+    $this->config()->protocol('http');
+    
   	$conf = new FileSystemConfiguration();
   	$conf->storeDir(__DIR__ . '/cdn');
   	$this->config()->staticFolder(__DIR__ . '/cdn');
@@ -37,10 +38,13 @@ class DemoHost extends AppHost {
   	$this->registerPlugin(new SpotEventsPlugin());
 
     $db = $container->get('OdmConfiguration');
-    $db->setDefaultDb('viseu-app');
-    $db->setHostname('ds1.codigo.ovh');
+    $db->setDefaultDb('viseuapp');
   }
 }
 
-$host = new DemoHost();
+$host = new LocalHost();
 $host->init();
+
+$crawler = $host->container()->get('Pi\ServiceInterface\WordpressCrawler');
+$crawler->fetch('http://noticiasdeviseu.com', 0, 50, 'Noticias de Viseu', 'http://noticiasdeviseu.com', 'http://www.diarioviseu.pt/logos/diario_viseu.png');
+$crawler->fetch('http://noticiasdeviseu.com', 50, 100, 'Noticias de Viseu', 'http://noticiasdeviseu.com', 'http://www.diarioviseu.pt/logos/diario_viseu.png');
